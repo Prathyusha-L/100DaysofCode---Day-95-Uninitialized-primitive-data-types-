ae10d653dcafd83be44d94f1a65995e3b5ea265c
L Prathyusha <83442712+Prathyusha-L@users.noreply.github.com>
2021-09-28 11:06:53 +0530

    Add files via upload

---
+++ Uninitialized primitive data types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// The following primitive data type variables will not
+	// be initialized with any default values
+	char ch;
+	float f;
+	int i;
+	double d;
+	long l;
+
+	cout << ch << endl;
+	cout << f << endl;
+	cout << i << endl;
+	cout << d << endl;
+	cout << l << endl;
+
+	return 0;
+}
